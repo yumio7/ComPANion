da366e12c4b9bf31ea8faa6277d12f61f87402ad
Connor Saila <csaila@live.com>
2023-02-18 20:09:22 -0500

    Update LevelManager.cs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class LevelManager : MonoBehaviour
 {
+    [SerializeField] private int hitPoints = 5;
+    [SerializeField] private float levelTime = 60.0f;
+
+    public bool isGameOver;
+    
+    private float _countdown;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isGameOver) return;
         
+        if (hitPoints <= 0)
+        {
+            isGameOver = true;
+            GameOver();
+        }
+
+        if (_countdown <= 0)
+        {
+            isGameOver = true;
+            GameWon();
+        }
+
+        _countdown -= Time.deltaTime;
+    }
+
+    private void GameOver()
+    {
+        
+    }
+
+    private void GameWon()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        hitPoints--;
     }
 }
