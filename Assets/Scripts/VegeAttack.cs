29eb1a23e49d1f3520b2b5f19a0ab4c439724567
Connor Saila <csaila@live.com>
2023-02-18 23:50:45 -0500

    Player Attack
    Day 1 done!!!

--- Assets/Scripts/VegeAttack.cs
+++ Assets/Scripts/VegeAttack.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class VegeAttack : MonoBehaviour
 {
-  private void OnTriggerEnter(Collider other)
+  private void OnTriggerEnter2D(Collider2D other)
   {
     if (!other.CompareTag("Enemy")) return;
     other.GetComponent<Enemy>().TakeDamage(1);
