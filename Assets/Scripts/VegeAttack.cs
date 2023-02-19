c039c99e6c181af2818a9832a30f473f6805b02c
Connor Saila <csaila@live.com>
2023-02-19 14:36:59 -0500

    AUDIO

--- Assets/Scripts/VegeAttack.cs
+++ Assets/Scripts/VegeAttack.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 
 public class VegeAttack : MonoBehaviour
 {
+  [SerializeField] private AudioClip hitSound;
+
   private void OnTriggerEnter2D(Collider2D other)
   {
     if (!other.CompareTag("Enemy")) return;
+    AudioSource.PlayClipAtPoint(hitSound, Camera.main.transform.position);
     other.GetComponent<Enemy>().TakeDamage(1);
   }
 }
--- Assets/Scripts/VegeParry.cs
+++ Assets/Scripts/VegeParry.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class VegeParry : MonoBehaviour
 {
+  [SerializeField] private AudioClip hitSound;
+  
   private void OnTriggerEnter2D(Collider2D other)
   {
     if (!other.CompareTag("Projectile")) return;
+    AudioSource.PlayClipAtPoint(hitSound, Camera.main.transform.position);
     other.GetComponent<ProjectileAttack>().Parry();
-
-    // TODO: que audio clip
   }
 }
